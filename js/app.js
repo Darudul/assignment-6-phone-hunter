53031136fc3e6b3c88927440fd1931caff4ddb10
Darudul <darudulislam@gmail.com>
2022-03-01 19:33:51 +0800

    add bonus part

--- js/app.js
+++ js/app.js
@@ -49,17 +49,40 @@ const getDEtails = (id) => {
 };
 
 const getIdByClick = (click) => {
-  console.log(click); 
+  console.log(click);
   const appendDiv = document.getElementById("append-id");
 
   const div = document.createElement("div");
   div.innerHTML = `
+  <div class='d-flex'>
+  <div class='col-4 col-lg-3 g-5 mb-5' >
   <img src="${click.image}" class="card-img-top" alt="..." />
-        <div class="card-body">
+  </div>
+        <div class="card-body mb-5">
           <h5 class="card-title">${click.name}</h5>
           <p class="card-text fw-bold">
             ${click.releaseDate}
           </p>
+          <p class="card-text "><span class='fw-bold' > Mainfeatures Information:</span> <br>
+          storage: ${click.mainFeatures.storage}<br>
+          chipSet: ${click.mainFeatures.chipSet} <br>
+          displaySize:  ${click.mainFeatures.displaySize} <br>
+          memory:  ${click.mainFeatures.memory} <br>
+          </p>
+
+          <p class="card-text"> <span class='fw-bold' > Sensors Information:</span> <br>
+            ${click.sensors}
+          </p>
+          <p class="card-text"> <span class='fw-bold' > Others Information:</span> <br>
+            ${click.others.WLAN} <br>
+            Bluetooth:  ${click.others.Bluetooth} <br>
+            GPS:  ${click.others.GPS} <br>
+            NFC:  ${click.others.NFC} <br>
+            Radio:  ${click.others.Radio} <br>
+            USB:  ${click.others.USB} <br>
+            WLAN:  ${click.others.WLAN} <br>
+          </p>
+        </div>
         </div>
   
   `;
